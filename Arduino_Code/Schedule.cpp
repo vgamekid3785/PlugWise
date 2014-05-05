dc3ebbf811df6b462723a51cfbb8f0fffc3f2e80
agalli93 <agalli93@gmail.com>
2014-05-05 19:45:59 -0400

    Additional comments added

--- Arduino_Code/Schedule.cpp
+++ Arduino_Code/Schedule.cpp
@@ -1,5 +1,6 @@
 #include "Schedule.h"
 
+//Function to change all the member variables
 void Schedule::create(const time_t new_time, const long new_inter, const unsigned int new_ticks, void (*function)()){
 	hit_time = new_time;
 	interval = new_inter;
@@ -7,6 +8,7 @@ void Schedule::create(const time_t new_time, const long new_inter, const unsigne
 	func = function;
 }
 
+//Function to check whether or not the function should be called
 void Schedule::check_time(){
 	//If current time is greater than start time && ticks > 0
 	if (now() >= hit_time && ticks > 0){
--- Arduino_Code/Schedule.h
+++ Arduino_Code/Schedule.h
@@ -9,15 +9,19 @@ Schedule(){
     unsigned int ticks = 0;
 }
 
-//Modifiers
+//Create function to change the values in the class
 void create(const time_t new_time, const long new_inter, const unsigned int new_ticks, void (*function)());
 
-//If the next function should go, if so, decrements ticks, moves start time and returns 1;
+//If the next function should go, if so, decrements ticks, moves start time
 void check_time();
 
 //Representation
+//Time that the function will be called
 time_t hit_time;
+//Interval for how often the function should be calles
 long interval;
+//Number of times the function should be called
 unsigned int ticks;
+//Function to be called
 void (*func)();
 };
